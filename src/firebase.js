95eb6f3a2f9a1c69083ddd9ea72d9f4b083ce8d5
jiyoungkimm <rla1014ss@gmail.com>
2023-02-01 14:53:26 +0900

    firebase.js 파일 생성

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXk-PXJ79DD2NfE15ZdLEPItmHd-m3vsM",
+  authDomain: "jiyoung-twitter.firebaseapp.com",
+  projectId: "jiyoung-twitter",
+  storageBucket: "jiyoung-twitter.appspot.com",
+  messagingSenderId: "977114574464",
+  appId: "1:977114574464:web:ffabfcdb544ba495c8c065",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
