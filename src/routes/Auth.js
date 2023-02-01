e6a74de73921abe1565ad76e1918e890e25dc3e7
jiyoungkimm <rla1014ss@gmail.com>
2023-02-01 14:33:14 +0900

    Auth.js 파일 초기 세팅

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onEmailChange = (event) => {
+    console.log(event);
+    const { value } = event.target;
+    setEmail(value);
+  };
+  const onPasswordChange = (event) => {
+    console.log(event);
+    const { value } = event.target;
+    setPassword(value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault(); // submit 버튼 클릭 시 새로고침 방지
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="input email"
+          value={email}
+          onChange={onEmailChange}
+          required
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="input password"
+          value={password}
+          onChange={onPasswordChange}
+          required
+        />
+        <input type="submit" value="sign in" />
+      </form>
+    </div>
+  );
+};
 
 export default Auth;
