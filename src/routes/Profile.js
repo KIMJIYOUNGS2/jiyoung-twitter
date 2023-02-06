4f03d4dad83301e5012060b25d97acfd312ff414
jiyoungkimm <rla1014ss@gmail.com>
2023-02-06 13:58:43 +0900

    Tweet, EditProfile 추가

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { auth } from "fbase";
+import { useNavigate } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  const navigate = useNavigate();
+
+  const onLogOutClick = () => {
+    console.log("logout click test");
+    auth.signOut();
+    navigate("/"); // Router.js
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
